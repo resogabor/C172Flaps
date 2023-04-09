d28fc24050e7f7013b6a2620bec3bfeed9ba5296
greso <greso@rslicence.hu>
2023-04-09 16:14:55 +0200

    Rectact, extend, incrase, decrase buttons

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ byte flaps0 = 23;
 byte flaps10 = 22;
 byte flaps20 = 21;
 byte flaps30 = 20;
-
+int lastButton;
 
 
 
@@ -18,29 +18,64 @@ void setup() {
   pinMode (flaps30, INPUT_PULLUP); 
 }
 
+void setButtonState(int buttonNum){
+  int diff = lastButton-buttonNum; 
+  if (diff > 0){
+    joy.setButton(2, 1);
+    delay(100);
+    joy.setButton(2, 0);        
+  }
+  if (diff < 0){
+    joy.setButton(1, 1);
+    delay(100);
+    joy.setButton(1, 0);    
+  }
+  lastButton = buttonNum;
+}
+
 void loop() {
   if(!digitalRead(flaps0)){
-    joy.setButton(0, 1);
+    delay(20);
+    if(!digitalRead(flaps0)){
+        if(lastButton != 0){
+        joy.setButton(0, 1);
+        lastButton = 0;
+      }else{
+        joy.setButton(0, 0);
+      }
+    }
   }else{
     joy.setButton(0, 0);
   }
 
   if(!digitalRead(flaps10)){
-    joy.setButton(1, 1);
-  }else{
-    joy.setButton(1, 0);
+    delay(20);
+    if(!digitalRead(flaps10)){
+      setButtonState(1);
+    }
   }
 
   if(!digitalRead(flaps20)){
-    joy.setButton(2, 1);
-  }else{
-    joy.setButton(2, 0);
+    delay(20);
+    if(!digitalRead(flaps20)){
+      setButtonState(2);
+    }
   }
 
   if(!digitalRead(flaps30)){
-    joy.setButton(3, 1);
+    delay(20);
+    if(!digitalRead(flaps30)){
+      if (lastButton != 3){
+        joy.setButton(3, 1);
+        lastButton = 3;
+      }else{
+        joy.setButton(3, 0);
+      }
+    }
   }else{
     joy.setButton(3, 0);
   }
 }
+
+
 
